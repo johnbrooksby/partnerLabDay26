cbe997a5487eb66d41b67f2872ec4e031294fa61
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-20 16:34:21 -0600

    Partner lab QA

---
+++ functions.test.js
@@ -0,0 +1,21 @@
+const {returnTwo, greeting, add } = require("./functions.js")
+
+test('returns the number 2', () => {
+    expect(returnTwo()).toEqual(2)
+})
+
+test('greeting should return Hello and the name input', () => {
+    expect(greeting('James')).toEqual('Hello, James')
+})
+
+test('greeting should return Hello and the name input', () => {
+    expect(greeting('Jill')).toEqual('Hello, Jill')
+})
+
+test('should add num1 and num2', () => {
+    expect(add(2, 5)).toEqual(7)
+})
+
+test('should add num1 and num2', () => {
+    expect(add(5, 9)).toEqual(14)
+})
